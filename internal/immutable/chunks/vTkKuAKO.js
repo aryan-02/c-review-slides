51315e837c0a873161fcdc9605aa46acf9a8dbc4
Aryan Mediratta <aryan_02@outlook.com>
2025-08-18 14:12:58 -0500

    Updates

---
+++ internal/immutable/chunks/vTkKuAKO.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1av1our)==null?void 0:s.base)??"/c-review-slides";var e;const t=((e=globalThis.__sveltekit_1av1our)==null?void 0:e.assets)??a;export{t as a,a as b};
